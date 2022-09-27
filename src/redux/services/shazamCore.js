460f858973091447107f39affa5dde0dc00b1934
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-09-27 05:40:23 +0500

    feat: add search component

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,28 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-
-// export const shazamCoreApi = createApi({
-//     reducerPath: 'shazamCoreApi',
-//     baseQuery: fetchBaseQuery({
-//         baseUrl: 'https://shazam-core.p.rapidapi.com/v1/',
-//         prepareHeaders: (headers) => {
-//             headers.set('X-RapidAPI-Key', '7340994c0amsh3735e0ca9e9180bp150245jsndb32e9acbc96');
-
-//             return headers;
-//         },
-//     }),
-//     endpoints: (builder) => ({
-//         getTopCharts: builder.query({ query: () => '/charts/world' }),
-//         getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?.track_id=${songid}`}),
-//         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-//     }),
-// })
-
-// export const {
-//     useGetTopChartsQuery,
-//     useGetSongDetailsQuery,
-//     useGetSongRelatedQuery
-// } = shazamCoreApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const shazamCoreApi = createApi({
